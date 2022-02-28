8431643134f56f9841bf3b705f8ef33df27f5de1
Peter <peter.w@droidtown.co>
2022-02-28 22:19:38 +0800

    上傳 Syntax101 教學練習用程式及說明

---
+++ Syntax101/week01/readme.md
@@ -0,0 +1,46 @@
+#Form: Words, Word Classes, Phrases
+
+本次的練習裡，將利用課堂中提到的「構詞上的証據」來做詞彙的詞性分類 (Word classification. a.k.a Part-of-Speech classification, word categorization)
+
+## 練習目標：
+
+1. 執行 **week01_Form_Words_WordClasses_Phrases.py** 並取得如下的輸出結果：
+```python
+Is 'glonk' a verb? True
+Is 'nurgy' an adjective? True
+Is 'bong' a noun? True
+Is 'ung' a preposition? True
+```
+
+>  操作方式：
+> 1. 用 WingIDE 打開 **week01_Form_Words_WordClasses_Phrases.py** 程式碼。
+> 2. 點擊畫面上方的綠色三角形，執行程式。
+> 3. 觀看畫面下方跳出的結果
+
+2. 將 `glonk`、`nurgy`、`bong` 和 `ung` 改成四個英文真正存在的詞彙。四個字的詞性分別為：動詞 see、形容詞 hungry、名詞 food 和介系詞 on。再做一次目標 1 的操作。取得如下的結果：
+```python
+Is 'see' a verb? False
+Is 'hungry' an adjective? False
+Is 'food' a noun? False
+Is 'on' a preposition? False
+```
+> 操作方式：
+> 1.  WingIDE 打開 **week01_Form_Words_WordClasses_Phrases.py** 程式碼。
+> 2. 把程式中，**第 177 行**「測試 'glonk' 是不是動詞」的 `unknownWord = "glonk"` 改成 `unknownWord = "see"`。如法泡製在其後的**第 186 行**、**第 193 行**和**第 200 行**。
+> 3. 點擊畫面上方的綠色三角形，執行程式。
+> 4. 觀看畫面下方跳出的結果
+
+## 討論：
+明明給的是 "see"、"hungry"、"food" 和 "on" 這四個詞性和拼字都正確的詞彙了。但是程式卻沒辦法正確地辨識出它們的詞性，這是因為我們給程式的參考資料只有 corpusLIST 裡的 12 個句子。
+
+換言之，程式只能從這 12 個句子做為素材，依據我們在 verbChecker()、nounChecker()、adjectiveChecker() 和 prepositionChecker() 中的演算法來推估它所收到的詞彙 (i.e., "see", "hungry", "food", "on") 是什麼詞性。
+
+如果這四個字剛好很少見於 corpusLIST 的話，那麼它的推估就是無效的了。
+
+改善的方法，就是給 corpusLIST 裡多增加一些含有 "see"、"hungry"、"food" 和 "on" …等等詞彙的英文句子。就像是給讓程式「多閱讀，多接受語言刺激」這麼一來，它就能學會更多詞彙的詞性囉。
+
+## 進階說明：
+程式結構如下流程說明。
+程式從進入點 **`if __name__ == "__main__":`** 開始執行。首先呼叫 `main()` 函式。而 `main()` 函式依指定的變數為 "verb", "noun", "adjective" 或 "preposition" 將資料傳給四個子函式各自進行運算後。取得結果為 True 或 False，並回傳給 `main()`。
+
+`main()` 將結果存入 `resultBOOL`之後，印出於畫面上。
---
+++ Syntax101/week01/week01_Form_Words_WordClasses_Phrases.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+
+def main(wordSTR="", wordClass="", refLIST=[]):
+    '''
+    main() is the main function of this program
+    input:
+        wordSTR   => string: the word to be tested
+        wordClass => string: "verb", "noun", "adjective", "preposition"
+        refLIST   => list: corpus of reference sentences
+    output:
+        True => boolean (the wordSTR belongs to the wordClass)
+        False => boolean (the wordSTR does not belong to the wordClass)
+    '''
+    if wordClass == "":
+        return "Please specify a word class. e.g., verb, noun, adjective, preposition"
+    else:
+        if wordClass in ("verb", "noun", "adjective", "preposition"):
+            pass
+        else:
+            return "Please specify a word class. e.g., verb, noun, adjective, preposition"
+
+    if refLIST == []:
+        return "Please provide sentences for word class alculation"
+    else:
+        pass
+
+    if wordClass == "verb":                                #wordClass="verb" => use verbChecker() to check if wordSTR is a verb.
+        resultBOOL = verbChecker(wordSTR, refLIST)
+    elif wordClass == "adjective":
+        resultBOOL = adjectiveChecker(wordSTR, refLIST)    #wordClass="adjective" => use adjectiveChecker() to check if wordSTR is an adjective.
+    elif wordClass == "noun":
+        resultBOOL = nounChecker(wordSTR, refLIST)         #wordClass="noun" => use nounChecker() to check if wordSTR is a noun.
+    else:
+        resultBOOL = prepositionChecker(wordSTR, refLIST)  #Check if wrodSTR is a preposition
+
+    return resultBOOL
+
+def verbChecker(wordSTR, refLIST):
+    '''
+    verbChecker() checks if the wordSTR is a verb with the refLIST as reference.
+    input:
+        wordSTR => string
+        refLIST => list
+    output:
+        True  => boolean (the wordSTR is a verb)
+        False => boolean (the wordSTR is not a verb)
+    '''
+    # Because an English verb may ends with no affix, with affix -s, with affix -ing and with affix -ed
+    # (Verbs with past {-ed}, participles {-ing}, {-en}, gerund {-ing}.), we design a checkingLIST as
+    # below and see if wordSTR, wordSTRs, wordSTRing, wordSTRed are in the refLIST.
+    # If they all of them can be found in the refLIST, it is very possible that the wordSTR is an English verb.
+
+    resultBOOL = True
+    for i in ("", "s", "ing", "ed", "en"): #Here lists all possible verb endings with/without affixes
+        if wordSTR+i in ",".join(refLIST):
+            pass
+        else:
+            resultBOOL = False
+    return resultBOOL
+
+def adjectiveChecker(wordSTR, refLIST):
+    '''
+    adjectiveChecker() checks if the wordSTR is a adjective with the refLIST as reference.
+    input:
+        wordSTR => string
+        refLIST => list
+    output:
+        True  => boolean (the wordSTR is an adjective)
+        False => boolean (the wordSTR is not an adjective)
+    '''
+    # Because an English adjective may ends with no affix, with affix -er, with affix -est and with affix -ly,
+    # we design a checkingLIST as below and see if wordSTR, wordSTRer, wordSTRest, wordSTRly are in the
+    # refLIST. If they all of them can be found in the refLIST, it is very possible that the wordSTR is
+    # an English verb.
+
+    if wordSTR.endswith("y"):            #This block deals with words ends with -y and turns it into -i. e.g., hungry -> hungri
+        wordSTRstem = wordSTR[:-1]+"i"
+    else:
+        wordSTRstem = wordSTR
+
+    counter = 0
+    if wordSTRstem in ",".join(refLIST):
+        counter = counter + 1
+
+    for i in ("er", "est", "ly"):
+        if wordSTRstem+i in ",".join(refLIST):
+            counter = counter + 1
+        else:
+            pass
+
+    if counter >= 2:
+        resultBOOL = True
+    else:
+        resultBOOL = False
+
+    return resultBOOL
+
+def nounChecker(wordSTR, refLIST):
+    '''
+    nounChecker() checks if the wordSTR is a noun with the refLIST as reference.
+    input:
+        wordSTR => string
+        refLIST => list
+    output:
+        True  => boolean (the wordSTR is a noun)
+        False => boolean (the wordSTR is not a noun)
+    '''
+    # Because an English noun may ends with no affix, with affix -s, with a determiner the/a/an goes before it,
+    # we design a checkingLIST as below and see if wordSTR, wordSTRs, the/a/an wordSTR are in the
+    # refLIST. If they all of them can be found in the refLIST, it is very possible that the wordSTR is
+    # an English noun.
+    resultBOOL = True
+    for i in ("", "s"):  # Nouns with plural {-s}.
+        if wordSTR+i in ",".join(refLIST):
+            pass
+        elif " a {} ".format(wordSTR) in ",".join(refLIST):
+            pass
+        elif " an {} ".format(wordSTR) in ",".join(refLIST):
+            pass
+        elif " the {} ".format(wordSTR) in ",".join(refLIST):
+            pass
+        else:
+            resultBOOL = False
+    return resultBOOL
+
+def prepositionChecker(wordSTR, refLIST):
+    '''
+    prepositionChecker() checks if the wordSTR is a preposition with the refLIST as reference.
+    input:
+        wordSTR => string
+        refLIST => list
+    output:
+        True  => boolean (the wordSTR is a preposition)
+        False => boolean (the wordSTR is not a preposition)
+    '''
+    # Because an English prepositions do not change its form in any contexts (Prepositions are morphologically inert.)
+    # we design a checkingLIST as below and see if a bare wordSTR exists and it cannot be a verb, a noun or an adjective.
+    # If it cannot be used as a verb, as a noun or as an adjective, it is very possible that the wordSTR is an English preposition.
+
+    resultBOOL = True
+    if verbChecker(wordSTR, refLIST):        #Use verbChecker() to see if it is morphologically like a verb.
+        resultBOOL = False
+    elif adjectiveChecker(wordSTR, refLIST): #Use adjectiveChecker() to see if it is morphologically like an adjective.
+        resultBOOL = False
+    elif nounChecker(wordSTR, refLIST):      #Use nounChecker() to see if it is morphologically like a noun.
+        resultBOOL = False
+    return resultBOOL
+
+
+
+if __name__ == "__main__":
+    # [注意] 本程式的設計目的在於簡單地示範如何利用 Python 程式語言來執行語言分析工作。本程式中的函式 (function)
+    # 仍需調整與深化以便在生產環境中使用。
+    # [Notice] This is a simple demonstration showing how to use Python to do linguistics,
+    # The functions provided may need further improvement for production purpose.
+
+    #[tw] 利用以下提供的語料 (corpus) 和前述的函式 (function) 計算輸入的「未知詞」的詞性
+    #[en] Determine categorical status of the unknown word with the corpus and the functions provided:
+    corpusLIST = ["John likes to glonk in the afternoons",
+                  "He never glonks on Sundays",
+                  "He started glonking when he was fourteen",
+                  "He once glonked an out-of-work actress",
+                  "He’s never glonked any of his classmates",
+                  "John was feeling nurgy, but happy",
+                  "He’s nurgier than anyone I know"
+                  "He’s been behaving very nurgily all week",
+                  "John is a bong, and so is Fred",
+                  "In fact, they’re both typical bongs",
+                  "She put the car ung the garage",
+                  "She made sure that it was right ung"
+                ]
+
+
+    #[tw] 測試 'glonk' 是不是動詞
+    #[en] Test if 'glonk' is a verb or not.
+    unknownWord = "glonk"
+    category = "verb"
+    resultBOOL = main(wordSTR=unknownWord, wordClass=category, refLIST=corpusLIST)
+    print("Is '{}' a {}? {}".format(unknownWord, category, resultBOOL))
+
+    #[tw] 測試 'nurgy' 是不是形容詞
+    #[en] Test if 'bong' is an adjective or not.
+    unknownWord = "nurgy"
+    category = "adjective"
+    resultBOOL = main(wordSTR=unknownWord, wordClass=category, refLIST=corpusLIST)
+    print("Is '{}' an {}? {}".format(unknownWord, category, resultBOOL))
+
+    #[tw] 測試 'bong' 是不是名詞
+    #[en] Test if 'bong' is a noun or not.
+    unknownWord = "bong"
+    category = "noun"
+    resultBOOL = main(wordSTR=unknownWord, wordClass=category, refLIST=corpusLIST)
+    print("Is '{}' a {}? {}".format(unknownWord, category, resultBOOL))
+
+    #[tw] 測試 'ung' 是不是介系詞
+    #[en] Test if 'ung' is a preposition or not.
+    unknownWord = "ung"
+    category = "preposition"
+    resultBOOL = main(wordSTR=unknownWord, wordClass=category, refLIST=corpusLIST)
+    print("Is '{}' a {}? {}".format(unknownWord, category, resultBOOL))
